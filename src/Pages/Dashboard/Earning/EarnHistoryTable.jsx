864c1bbb6ce6778c6b93e758aff6fdcca52a1dde
siffahim <siffahim25@gmail.com>
2023-08-23 11:18:52 +0600

    drawer design

--- src/Pages/Dashboard/Earning/EarnHistoryTable.jsx
+++ src/Pages/Dashboard/Earning/EarnHistoryTable.jsx
@@ -1,59 +1,8 @@
-import { Table } from "antd";
-import React from "react";
+import { Button, Drawer, Table } from "antd";
+import React, { useState } from "react";
 import { AiOutlinePrinter } from "react-icons/ai";
 import { LiaSaveSolid } from "react-icons/lia";
-
-const columns = [
-  {
-    title: "TRIP NO.",
-    dataIndex: "tripNo",
-    key: "tripNO",
-  },
-  {
-    title: "TIME",
-    dataIndex: "time",
-    key: "time",
-    responsive: ["md"],
-  },
-  {
-    title: "USER NAME",
-    dataIndex: "username",
-    key: "username",
-    responsive: ["lg"],
-  },
-  {
-    title: "METHOD",
-    dataIndex: "method",
-    key: "method",
-  },
-  {
-    title: "AMOUNT",
-    dataIndex: "amount",
-    key: "amount",
-    responsive: ["md"],
-  },
-  {
-    title: "STATUS",
-    dataIndex: "status",
-    key: "status",
-  },
-  {
-    title: "PRINT/VIEW",
-    dataIndex: "printView",
-    key: "printView",
-    responsive: ["lg"],
-    render: () => (
-      <div style={{ textAlign: "center" }}>
-        <a style={{ marginRight: "12px" }}>
-          <AiOutlinePrinter style={{ fontSize: "30px", color: "#999999" }} />
-        </a>
-        <a>
-          <LiaSaveSolid style={{ fontSize: "30px", color: "#999999" }} />
-        </a>
-      </div>
-    ),
-  },
-];
+import DrawerPage from "../../../Components/DrawerPage/DrawerPage";
 
 const data = [
   {
@@ -218,5 +167,90 @@ const data = [
   },
 ];
 
-const EarnHistoryTable = () => <Table columns={columns} dataSource={data} />;
+const EarnHistoryTable = () => {
+  const columns = [
+    {
+      title: "TRIP NO.",
+      dataIndex: "tripNo",
+      key: "tripNO",
+    },
+    {
+      title: "TIME",
+      dataIndex: "time",
+      key: "time",
+      responsive: ["md"],
+    },
+    {
+      title: "USER NAME",
+      dataIndex: "username",
+      key: "username",
+      responsive: ["lg"],
+    },
+    {
+      title: "METHOD",
+      dataIndex: "method",
+      key: "method",
+    },
+    {
+      title: "AMOUNT",
+      dataIndex: "amount",
+      key: "amount",
+      responsive: ["md"],
+    },
+    {
+      title: "STATUS",
+      dataIndex: "status",
+      key: "status",
+    },
+    {
+      title: "PRINT/VIEW",
+      dataIndex: "printView",
+      key: "printView",
+      responsive: ["lg"],
+      render: (
+        _,
+        record // Use the second parameter 'record'
+      ) => (
+        <div style={{ textAlign: "center" }}>
+          <Button type="text" style={{ marginRight: "10px" }}>
+            <AiOutlinePrinter style={{ fontSize: "30px", color: "#999999" }} />
+          </Button>
+          <Button onClick={() => showDrawer(record)} type="text">
+            <LiaSaveSolid style={{ fontSize: "30px", color: "#999999" }} />
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
+  const [isDrawerVisible, setIsDrawerVisible] = useState(false);
+  const [drawerData, setDrawerData] = useState(null);
+
+  const showDrawer = (record) => {
+    setIsDrawerVisible(true);
+    setDrawerData(record);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerVisible(false);
+    setDrawerData(null);
+  };
+
+  return (
+    <div>
+      <Table columns={columns} dataSource={data} />
+      <Drawer
+        title="Drawer Title"
+        placement="right"
+        closable={false}
+        onClose={closeDrawer}
+        visible={isDrawerVisible}
+        width={400}
+      >
+        {drawerData && <DrawerPage drawerData={drawerData} />}
+      </Drawer>
+    </div>
+  );
+};
+
 export default EarnHistoryTable;
