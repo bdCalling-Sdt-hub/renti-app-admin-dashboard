1887f79c450cc4ae552ea061421e4420b171ce54
siffahim <siffahim25@gmail.com>
2023-08-23 18:33:35 +0600

    host page design

--- src/Components/DrawerPage/DrawerPage.jsx
+++ src/Components/DrawerPage/DrawerPage.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable no-unsafe-optional-chaining */
 /* eslint-disable react/prop-types */
-import { Badge, Button, Typography } from "antd";
+import { Badge, Button, Input, Typography } from "antd";
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import cardImg from "../../Images/Cards.png";
 const { Title } = Typography;
 
 const DrawerPage = (props) => {
-  console.log(props.earningData);
-  const { username, status } = props?.earningData;
   return (
     <>
       {props.earningData && (
@@ -17,9 +16,9 @@ const DrawerPage = (props) => {
               <img width={120} src="https://i.imgur.com/JFHjdNr.jpg" alt="" />
             </div>
             <div style={{ marginTop: "-7px" }}>
-              <p style={{ fontSize: "20px" }}>{username}</p>
+              <p style={{ fontSize: "20px" }}>{props.earningData.username}</p>
               <p>INE: SNHRM570818MDFPM10</p>
-              <p>Trip Completes:{status.length}</p>
+              <p>Trip Completes:{props.earningData.status.length}</p>
               <div
                 style={{ display: "flex", alignItems: "center", gap: "2px" }}
               >
@@ -67,6 +66,97 @@ const DrawerPage = (props) => {
               }}
             >
               Print
+            </Button>
+          </div>
+        </div>
+      )}
+      {props.hostData && (
+        <div>
+          <div
+            style={{
+              display: "flex",
+              gap: "15px",
+              borderBottom: "1px solid #B0B3DD",
+            }}
+          >
+            <div>
+              <img width={120} src="https://i.imgur.com/JFHjdNr.jpg" alt="" />
+            </div>
+            <div style={{ marginTop: "-7px" }}>
+              <p style={{ fontSize: "20px" }}>{props.hostData?.name}</p>
+              <p>INE: SNHRM570818MDFPM10</p>
+              <p>Trip Completes: 5</p>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "2px" }}
+              >
+                <AiFillStar color="#fba91d" />
+                <span>4.8</span>
+              </div>
+            </div>
+          </div>
+
+          <div style={{ marginTop: "10px" }}>
+            <div style={{ marginBottom: "15px" }}>
+              <label htmlFor="">Email</label>
+              <Input readOnly style={{ height: "45px" }} />
+            </div>
+            <div style={{ marginBottom: "15px" }}>
+              <label htmlFor="">Phone</label>
+              <Input readOnly style={{ height: "45px" }} />
+            </div>
+            <div style={{ marginBottom: "15px" }}>
+              <label htmlFor="">Address</label>
+              <Input readOnly style={{ height: "45px" }} />
+            </div>
+          </div>
+
+          <div style={{ marginTop: "30px" }}>
+            <h3>Payment Method</h3>
+            <img width="80" style={{ margin: "10px 0" }} src={cardImg} alt="" />
+            <div style={{ marginTop: "10px" }}>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="">Credit Card Number</label>
+                <Input readOnly style={{ height: "45px" }} />
+              </div>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="">Account Number</label>
+                <Input readOnly style={{ height: "45px" }} />
+              </div>
+              <div style={{ marginBottom: "15px" }}>
+                <label htmlFor="">Account Holder Name</label>
+                <Input readOnly style={{ height: "45px" }} />
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: 10,
+              position: "absolute",
+              bottom: 10,
+            }}
+          >
+            <Button
+              block
+              style={{
+                background: "#000B90",
+                color: "white",
+                height: 50,
+                width: "270px",
+              }}
+            >
+              Block
+            </Button>
+            <Button
+              block
+              style={{
+                background: "#000B90",
+                color: "white",
+                height: 50,
+                width: "270px",
+              }}
+            >
+              Delete
             </Button>
           </div>
         </div>
