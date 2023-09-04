9ad1fb08c2cf21bd38c1ed9c5d142363276c2340
unknown <freelancerrtushar@gmail.com>
2023-09-04 16:15:08 +0600

    privacy policy page functionality complete

--- src/Components/Setting/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Components/Setting/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,16 +1,59 @@
 import React, { useState, useRef, useMemo } from 'react';
 import JoditEditor from 'jodit-react';
 import { Button, Col, Row } from 'antd';
+import { useEffect } from 'react';
+import axios from '../../../../Config';
+import Swal from 'sweetalert2';
 
 const PrivacyPolicy = () => {
 
   const editor = useRef(null)
   const [content, setContent] = useState('');
 
-  const aboutDataSave = () => {
-    alert(content);
+  const privacyAndPolicyDataSave = () => {
+
+    let token=localStorage.getItem("token");
+    let data={
+      content
+    }
+
+    axios.post("/api/privacyPolicy/create",data,{
+      headers:{
+        
+        "authorization":`Bearer ${token}`
+      }
+    }).then(res=>{
+      Swal.fire(
+        'Good job!',
+        res.data.message,
+        'success'
+      )
+    }).catch(err=>{
+      Swal.fire(
+        'Oops!',
+         err.response.data.message,
+        'error'
+      )
+    });
 
   }
+
+  useEffect(()=>{
+    let token=localStorage.getItem("token");
+      axios.get("/api/privacyPolicy/all",{
+        headers:{
+          "Content-Type":"application/json",
+          "authorization":`Bearer ${token}`
+        }
+      }).then(res=>{
+     
+        setContent(res.data.privacyPolicy.content)
+      }).catch(err=>console.log(err))
+  },[])
+
+
+
+
   return (
     <div>
       
@@ -24,7 +67,7 @@ const PrivacyPolicy = () => {
             onChange={newContent => { setContent(newContent) }}
           />
 
-          <Button onClick={aboutDataSave} block style={{ marginTop: "30px", backgroundColor: "#000b90", color: "#fff", height: "50px" }}>save</Button>
+          <Button onClick={privacyAndPolicyDataSave} block style={{ marginTop: "30px", backgroundColor: "#000b90", color: "#fff", height: "50px" }}>save</Button>
 
         </Col>
          
