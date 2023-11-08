17b86e7231a53104b012466c52193e9a5c543396
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2023-11-08 18:13:54 +0600

    some changes

--- src/Pages/Dashboard/Notification/Notification.jsx
+++ src/Pages/Dashboard/Notification/Notification.jsx
@@ -158,12 +158,18 @@ function Notification() {
         <div>
           <h2 style={{ marginBottom: "10px" }}>{modalData?.message}</h2>
 
+          <div>
           <img
             style={{ borderRadius: "10px" }}
-            width="100%"
+            width="50%"
             src={modalData?.image}
             alt=""
           />
+
+          <div className="">
+            
+          </div>
+            </div>
         </div>
       </Modal>
     </div>
