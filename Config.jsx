e8462a8e23b2c1dafef8011da1aee542b6ec68c8
Fahim <siffahim25@gmail.com>
2023-11-16 15:00:45 +0600

    renti:

--- Config.jsx
+++ Config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://192.168.10.14:3001",
+  baseURL: "http://104.131.72.121:3000",
   timeout: 10000,
   headers: { "X-Custom-Header": "foobar" },
 });
